1b9d0f50855f641917f6127596173cca913b79f8
JakeVanCleave <jake@jake>
2023-03-30 10:46:15 -0700

    I had weird wording just fixed some

--- lib/classes/review.py
+++ lib/classes/review.py
@@ -25,7 +25,7 @@ class Review:
     @customer.setter
     def customer(self, value):
         if not isinstance(customer, Customer):
-            raise Exception("Customer must be of type Customer")
+            raise Exception("Customer must be of type customer")
         self._customer = value
  
     @property
@@ -35,7 +35,7 @@ class Review:
     @restaurant.setter
     def restaurant(self, value):
         if not isinstance(restaurant, Restaurant):
-            raise Exception("Restaurant must be of type Restaurant")
+            raise Exception("Restaurant must be of type restaurant")
         self._restaurant = value
 
     @property
@@ -45,7 +45,7 @@ class Review:
     @rating.setter
     def rating(self, value):
         if not 1 <= value <= 5:
-            raise Exception("Rating must be a number between 1 and 5, inclusive")
+            raise Exception("Rating must be a number between 1 and 5")
         self._rating = value
 
     
